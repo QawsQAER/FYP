1e02ba1124ecef755101e59f1c1e6a62e75f35a8
Edward <huangxx_2155@qq.com>
2014-01-14 23:20:47 +0800

    release a formal version of testnode.cpp

--- Simulation/TreeStructure/testnode.cpp
+++ Simulation/TreeStructure/testnode.cpp
@@ -15,16 +15,16 @@ int main(int argc, char ** argv)
 	else
 	{
 		printf("Error: invalid number of arguments\n");
+		printf("Usage: testnode [number of tree] [maximum child of a node]\n");
 		return 0;
 	}
-	for(int count = 1; count < num_of_node/2;count++)
-	{
-	TreeStructure Tree(count);
+
+	TreeStructure Tree(max_child);
 	uint32_t ID = 0;
 	for(ID = 1;ID < num_of_node;ID++)
 		Tree.add_node(ID);
 //	Tree.display_tree();
 	printf("the maximum delay of this tree with %d max child is %d\n\n",Tree.get_max(),Tree.get_max_delay());
-	}
+
 	return 0;
 }
