a7f47c72be8f863ff51c2068360ad511ac7f0ffd
Edward <huangxx_2155@qq.com>
2014-01-14 19:58:31 +0800

    1. comment out int8_t in TYPEDEF.h because it has collision with /usr/include/sys/type.h 2. The testnode.cpp now takes two arguments, one as the number of node the other as the maximum number of children

--- Simulation/TYPEDEF.h
+++ Simulation/TYPEDEF.h
@@ -1,3 +1,5 @@
+
+//#ifndef _SYS_TYPES_H
 #ifndef _TYPEDEF_H
 #define _TYPEDEF_H
 #include <cstddef>
@@ -6,6 +8,8 @@ typedef int int32_t;
 typedef unsigned short uint16_t;
 typedef short int16_t;
 typedef unsigned char uint8_t;
-typedef char int8_t;
+//typedef char int8_t;
 
 #endif
+//#endif
+
--- Simulation/TreeStructure/testnode.cpp
+++ Simulation/TreeStructure/testnode.cpp
@@ -1,20 +1,30 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "node.h"
 #include "TreeStructure.h"
-int main()
+int main(int argc, char ** argv)
 {
 
-	TreeStructure Tree;
-	uint8_t ID = 0;
-	for(ID = 1;ID < 10;ID++)
+	uint32_t num_of_node;
+	unsigned char max_child;
+	if(argc == 3)
+	{
+		num_of_node = atoi(argv[1]);
+		max_child = atoi(argv[2]);
+	}
+	else
+	{
+		printf("Error: invalid number of arguments\n");
+		return 0;
+	}
+	for(int count = 1; count < num_of_node/2;count++)
+	{
+	TreeStructure Tree(count);
+	uint32_t ID = 0;
+	for(ID = 1;ID < num_of_node;ID++)
 		Tree.add_node(ID);
-	Tree.display_tree();
-	printf("the maximum delay of this tree is %d\n\n\n",Tree.get_max_delay());
-	
-	TreeStructure Tree2(3);	
-	for(ID = 1;ID < 10;ID++)
-	Tree2.add_node(ID);
-	Tree2.display_tree();
-	printf("the maximum delay of this tree is %d\n",Tree2.get_max_delay());
+//	Tree.display_tree();
+	printf("the maximum delay of this tree with %d max child is %d\n\n",Tree.get_max(),Tree.get_max_delay());
+	}
 	return 0;
 }
